cca6fd352750cba84a2308f34ba9d67f4d57b721
Shivansh Goel <shivanshgoel007@gmail.com>
2021-02-20 23:26:41 +0530

    Added backup feature

--- app/src/main/java/com/goel/attendancetracker/BackupRestoreActivity.java
+++ app/src/main/java/com/goel/attendancetracker/BackupRestoreActivity.java
@@ -1,12 +1,17 @@
 package com.goel.attendancetracker;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.goel.attendancetracker.database.Params;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -14,13 +19,17 @@ import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.Objects;
 
 public class BackupRestoreActivity extends AppCompatActivity {
 
     public static FirebaseUser user;
-    public static String SIGN_OUT = "com.goel.attendancetracker.signout";
+    public static final String SIGN_OUT = "com.goel.attendancetracker.signout";
+    private StorageReference storageRef;
 
 
     @Override
@@ -30,6 +39,7 @@ public class BackupRestoreActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setTitle("Backup & Restore");
         ((TextView) findViewById(R.id.user_email)).setText(user.getEmail());
 
+        storageRef = FirebaseStorage.getInstance().getReference(user.getUid() + "/" + Params.DB_NAME);
         setAds();
         // Implementing ads
         MobileAds.initialize(this, initializationStatus -> {
@@ -87,6 +97,32 @@ public class BackupRestoreActivity extends AppCompatActivity {
     }
 
     public void createBackup(View view){
+        new AlertDialog.Builder(BackupRestoreActivity.this)
+                .setTitle("Confirm Backup")
+                .setMessage("Creating the backup will overwrite the existing backup. Do want to continue?")
+                .setPositiveButton("Confirm", (dialog, which) -> {
+                    dialog.dismiss();
+                    backupDatabase();
+                })
+
+                .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    private void backupDatabase(){
+        ConstraintLayout loading = findViewById(R.id.backup_restore_loading);
+        loading.setVisibility(View.VISIBLE);
+        Uri databaseUri = Uri.fromFile(getDatabasePath(Params.DB_NAME));
+        UploadTask backupTask = storageRef.putFile(databaseUri);
+        backupTask.addOnSuccessListener(taskSnapshot -> {
+            Toast.makeText(BackupRestoreActivity.this, "Backed Up Successfully", Toast.LENGTH_LONG).show();
+            loading.setVisibility(View.INVISIBLE);
+        });
+        backupTask.addOnFailureListener(e -> {
+            Toast.makeText(BackupRestoreActivity.this, "Back Up Failed", Toast.LENGTH_LONG).show();
+            loading.setVisibility(View.INVISIBLE);
+        });
 
     }
 
